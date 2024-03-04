b07ef7cea92ff2e9b359b4a1e068f712cf3ad973
WOLFIE_OG <oliverjtodd@yahoo.com>
2024-03-04 18:59:42 +0000

    Fix

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: otodd <otodd@student.42london.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 15:06:06 by otodd             #+#    #+#             */
-/*   Updated: 2024/03/04 17:04:37 by otodd            ###   ########.fr       */
+/*   Updated: 2024/03/04 17:47:19 by otodd            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,10 @@ int	main(int arg_n, char **arg_a)
 
 	variant = 0;
 	name = "map.ber";
-	if (arg_n == 2)
+	if (arg_n >= 2)
 		name = arg_a[1];
-	if (arg_n == 3)
+	ft_printf("%d\n", arg_n);
+	if (arg_n >= 3)
 		if (ft_ischeck_str(arg_a[2], ft_isdigit))
 			variant = ft_atoi(arg_a[2]);
 	c = init_main(name, variant);
